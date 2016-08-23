00fc9d44f48a32f9890dc5378ac137e4a3135c62
B-Roxy <roxy-girl@yandex.ru>
2016-08-23 17:46:03 +0700

    add RecursiveSorting

--- Sortings.cpp
+++ Sortings.cpp
@@ -4,22 +4,24 @@
 #include <iostream>
 #include <iomanip>
 #include <memory>
+#include <cctype>
 
 using namespace std;
 
 const int countData = 7;
-const int countSort = 3; 
+const int countSort = 4; 
 
 enum Sortings {
 	ChoiceSort,
 	BubbleSort,
-	ShakerSort
+	ShakerSort, 
+	RecursiveSort
 };
 
 struct StatSorting{
 	float time;
-	int countCompare;	// Г±Г°Г ГўГ­ГҐГ­ГЁГї
-	int countSwap;		// ГЇГҐГ°ГҐГ±ГІГ Г­Г®ГўГЄГЁ
+	int countCompare;	// сравнения
+	int countSwap;		// перестановки
 };
 
 struct Statistics {
@@ -27,9 +29,9 @@ struct Statistics {
 	StatSorting dataSorting[countData];
 } dataStatSorting[countSort];
 
-/* Г±Г®Г°ГІГЁГ°Г®ГўГЄГ  ГўГ»ГЎГ®Г°Г®Г¬
-1) ГЁГ№ГҐГ¬ Г¬ГЁГ­ГЁГ¬Г Г«ГјГ­Г»Г© ГўГ® ГўГ±ГҐГ¬ Г¬Г Г±Г±ГЁГўГҐ Г± ГІГҐГЄГіГ№ГҐГЈГ® ГЇГ®Г«Г®Г¦ГҐГ­ГЁГї ГЁ Г¤Г® ГЄГ®Г­Г¶Г  Г¬Г Г±Г±ГЁГўГ , 
-2) ГЇГҐГ°ГҐГ±ГІГ ГўГ«ГїГҐГ¬ Г¬ГЁГ­ГЁГ¬Г Г«ГјГ­Г»Г© Г± ГІГҐГЄГіГ№ГЁГ¬ Г·Г«ГҐГ­Г®Г¬ Г¬Г Г±Г±Г ГЁГўГ 
+/* сортировка выбором
+1) ищем минимальный во всем массиве с текущего положения и до конца массива, 
+2) переставляем минимальный с текущим членом массаива
 */
 void ChoiceSorting(int *arr, int size, int indx) {
 	int min, c;
@@ -59,7 +61,7 @@ void ChoiceSorting(int *arr, int size, int indx) {
 	dataStatSorting[ChoiceSort].dataSorting[indx].countSwap = countSwap;
 }
 
-/* Г±Г®Г°ГІГЁГ°Г®ГўГЄГ  ГЇГіГ§Г»Г°ГјГЄГ®Г¬
+/* сортировка пузырьком
 */
 void BubbleSorting(int *arr, int size, int indx) {
 	clock_t start;
@@ -71,6 +73,7 @@ void BubbleSorting(int *arr, int size, int indx) {
 	while (check) {
 		check = false;
 		for (int i = 0; i < size - 1; i++) {
+			countCompare++;
 			if (arr[i] > arr[i + 1]) {
 				int c = arr[i];
 				arr[i] = arr[i+1];
@@ -78,9 +81,8 @@ void BubbleSorting(int *arr, int size, int indx) {
 				check = true;
 				countSwap++;
 			}
-			countCompare++;
 		}
-		size--; // Г±Г¤ГўГЁГЈГ ГҐГ¬ ГЇГ°Г ГўГіГѕ ГЈГ°Г Г­ГЁГ¶Гі, ГІ.ГЄ. Г±Г Г¬Г»Г© ГЎГ®Г«ГјГёГ®Г© ГЅГ«-ГІ ГЎГіГ¤ГҐГІ Г±Г¤ГўГЁГ­ГіГІ ГўГЇГ°Г ГўГ®
+		size--; // сдвигаем правую границу, т.к. самый большой эл-т будет сдвинут вправо
 	} 
 	dataStatSorting[BubbleSort].name = "BubbleSort";
 	dataStatSorting[BubbleSort].dataSorting[indx].time = (clock() - start) / (float)CLK_TCK;
@@ -112,22 +114,26 @@ void ShakerSorting(int *arr, int size, int indx) {
 		check = false;
 		if (isRight) {
 			for (int i = a; i < b - 1; i++) {
+				countCompare++;
 				if (arr[i] > arr[i + 1]){
 					int c = arr[i];
 					arr[i] = arr[i+1];
 					arr[i+1] = c;
 					check = true;
+					countSwap++;
 				}
 			}
 			b--;
 		}
 		else {
 			for (int i = b - 1; i > a; i--) {
+				countCompare++;
 				if (arr[i] < arr[i - 1]) {
 					int c = arr[i];
 					arr[i] = arr[i-1];
 					arr[i-1] = c;
 					check = true;
+					countSwap++;
 				}
 			}
 			a++;
@@ -140,17 +146,77 @@ void ShakerSorting(int *arr, int size, int indx) {
 	dataStatSorting[ShakerSort].dataSorting[indx].countSwap = countSwap;
 }
 
+void RecursiveSorting(int *arr, int a, int b, int indx) {
+
+	if (b - a > 1) {
+		RecursiveSorting(arr, a, a + (b-a)/2, indx);
+		RecursiveSorting(arr, a+ (b-a)/2, b, indx);
+	}
+	int *arr2 = new int[b-a];
+	int i = 0,
+		i1 = a,
+		i2 = (b-a)/2 + a;
+	while (i1 < a + (b - a) / 2 && i2 < b) {
+		if (arr[i1] < arr[i2]) {
+			arr2[i++] = arr[i1++];
+		}
+		else {
+			arr2[i++] = arr[i2++];
+		}
+	}
+	while (i1 < a + (b-a)/2) {
+		arr2[i++] = arr[i1++];
+	}
+	while (i2 < b) {
+		arr2[i++] = arr[i2++];
+	}
+	for (int i = 0; i < b - a; i++) {
+		arr[a+i] = arr2[i];
+	}
+	delete [] arr2;
+}
+
+void EntryRecursiveSorting(int *arr, int size, int indx) {
+	clock_t start;
+	static int countSwap = 0;
+	static int countCompare = 0;
+
+	start = clock();
+	RecursiveSorting(arr, 0, size, indx);
+	dataStatSorting[RecursiveSort].name = "RecursiveSort";
+	dataStatSorting[RecursiveSort].dataSorting[indx].time = (clock() - start) / (float)CLK_TCK;
+	dataStatSorting[RecursiveSort].dataSorting[indx].countCompare = countCompare;
+	dataStatSorting[RecursiveSort].dataSorting[indx].countSwap = countSwap;
+}
+
 void PrintData(int size) {
-	// ГўГ»ГўГ®Г¤ Г°ГҐГ§ГіГ«ГјГІГ ГІГ®Гў
+	//
+	/*if (size <= 10000) {
+		cout << size << endl;
+		cout << left << "Sort" << right << setw(30) << "Compare / Swap" << endl; 
+		cout << "\t\tdata1\tdata2\tdata3\tdata4\tdata5\tdata6\tdata7	\n";
+		for (int i = 0; i < countSort; i++) {
+			cout << setw(13) << setfill(' ') <<left << dataStatSorting[i].name  << "\t";
+			for (int j = 0; j < countData; j++) {
+				cout << dataStatSorting[i].dataSorting[j].countCompare;
+				cout << " / ";
+				cout << dataStatSorting[i].dataSorting[j].countSwap;
+			}
+			cout << setw(70) <<setfill('-') << endl;
+		}
+	}
+	*/
 	cout << size << endl;
 	cout << "Sort\t\tdata1\tdata2\tdata3\tdata4\tdata5\tdata6\tdata7	\n";
 	for (int i = 0; i < countSort; i++) {
-		cout << dataStatSorting[i].name << "\t";
+		cout << setw(13) << setfill(' ') << left << dataStatSorting[i].name  << "\t";
 		for (int j = 0; j < countData; j++) {
 			cout << setw(8) << left << dataStatSorting[i].dataSorting[j].time;
 		}
 		cout << endl;
 	}
+	cout << endl << endl;
+	
 	cout << endl << endl;
 }
 
@@ -167,14 +233,14 @@ void main() {
 			arr[j] = rand() % sizes[i];
 		}
 		for (int j = 0; j < countData; j++) {
-			// Г¤Г Г­Г­Г»ГҐ:
-			//0 - Г±Г«ГіГ·Г Г©Г­Г» ГЁ Г­ГҐГ®ГІГ±Г®Г°ГІГЁГ°Г®ГўГ Г­Г­Г»
-			//1 - Г®ГІГ±Г®Г°ГІГЁГ°Г®ГўГ Г­Г­Г»ГҐ ГЇГ® ГўГ®Г§Г°Г Г±ГІГ Г­ГЁГѕ
-			//2 - Г®ГІГ±Г®Г°ГІГЁГ°Г®ГўГ Г­Г­Г»ГҐ ГЇГ® ГіГЎГ»ГўГ Г­ГЁГѕ
-			//3 - ГЇГ® ГўГ®Г§Г°Г Г±ГІГ Г­ГЁГѕ, Г§Г  ГЁГ±ГЄГ«ГѕГ·ГҐГ­ГЁГҐГ¬ ГЅГ«ГҐГ¬ГҐГ­ГІГ  Г± ГЁГ­Г¤ГҐГЄГ±Г®Г¬  (size/4)
-			//4 - ГЇГ® ГўГ®Г§Г°Г Г±ГІГ Г­ГЁГѕ, Г§Г  ГЁГ±ГЄГ«ГѕГ·ГҐГ­ГЁГҐГ¬ ГЅГ«ГҐГ¬ГҐГ­ГІГ  Г± ГЁГ­Г¤ГҐГЄГ±Г®Г¬  3*(size/4)
-			//5 - ГЇГ® ГіГЎГ»ГўГ Г­ГЁГѕ, Г§Г  ГЁГ±ГЄГ«ГѕГ·ГҐГ­ГЁГҐГ¬ ГЁГ­Г¤ГҐГЄГ±Г®Гў Г¬Г Г±Г±ГЁГўГ®Гў (size/4)
-			//6 - ГЇГ® ГіГЎГ»ГўГ Г­ГЁГѕ, Г§Г  ГЁГ±ГЄГ«ГѕГ·ГҐГ­ГЁГҐГ¬ ГЁГ­Г¤ГҐГЄГ±Г®Гў Г¬Г Г±Г±ГЁГўГ®Гў 3*(size/4) 
+			// данные:
+			//0 - случайны и неотсортированны
+			//1 - отсортированные по возрастанию
+			//2 - отсортированные по убыванию
+			//3 - по возрастанию, за исключением элемента с индексом  (size/4)
+			//4 - по возрастанию, за исключением элемента с индексом  3*(size/4)
+			//5 - по убыванию, за исключением индексов массивов (size/4)
+			//6 - по убыванию, за исключением индексов массивов 3*(size/4) 
 			if (j == 2 || j == 5 || j == 6)
 				ReverseData(arr, sizes[i]);
 			if (j == 3 || j == 5)
@@ -187,6 +253,8 @@ void main() {
 			BubbleSorting(arr, sizes[i], j);
 			memcpy(arr, tmpArr, sizeof(int) * sizes[i]);
 			ShakerSorting(arr, sizes[i], j);
+			memcpy(arr, tmpArr, sizeof(int) * sizes[i]);
+			EntryRecursiveSorting(arr, sizes[i], j);
 		}
 		delete[] arr;
 		delete[] tmpArr;
